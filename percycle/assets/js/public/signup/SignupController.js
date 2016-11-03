0ab4c919906fedf838a8ae53357ae6904a2bf7b2
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2016-11-03 01:32:39 -0200

    Desenvolvimento da lógica do botão 'Criar Conta'

--- percycle/assets/js/public/signup/SignupController.js
+++ percycle/assets/js/public/signup/SignupController.js
@@ -1,5 +1,14 @@
 angular.module('SignupModule').controller('SignupController', ['$scope', function($scope){
+    
+    //Nessa parte do código estou setando o estado do formulário:
+    $scope.signupForm = {
+        loading: false
+    }
+
     submitSignupForm = function() {
+        //Caso todos os inputs seguem dentro dos padrões, retorno o estado do formulário como true:
+        $scope.signupForm.loading = true;
+
         console.log("Boa! Deu Tudo certo!!!");
     }
 }]);
