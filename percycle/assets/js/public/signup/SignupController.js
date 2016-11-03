79350e830dde761fc47d448dd810a33cdcb7717c
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2016-11-03 01:45:10 -0200

    Desenvolvimento da lógica do envio das infos do formulário via Angular.

--- percycle/assets/js/public/signup/SignupController.js
+++ percycle/assets/js/public/signup/SignupController.js
@@ -1,14 +1,30 @@
-angular.module('SignupModule').controller('SignupController', ['$scope', function($scope){
+angular.module('SignupModule').controller('SignupController', ['$scope', '$http', function($scope, $http){
     
     //Nessa parte do código estou setando o estado do formulário:
     $scope.signupForm = {
         loading: false
     }
 
-    submitSignupForm = function() {
+    $scope.submitSignupForm = function() {
         //Caso todos os inputs seguem dentro dos padrões, retorno o estado do formulário como true:
         $scope.signupForm.loading = true;
 
-        console.log("Boa! Deu Tudo certo!!!");
+        //Aqui estou enviando um requisição do tipo 'POST' dos campos preenchidos no formulário:
+        $http.post('/signup', {
+            name: $scope.signupForm.name,
+            email: $scope.signupForm.email,
+            password: $scope.signupForm.password
+        })
+        //Se tudo der certo... encaminharei as informações para a rota: 'User':
+        .then(function onSuccess(){
+            window.location = '/user'
+        })
+        //Caso dê algum erro... retornará para o usuário uma mensagem de erro:
+        .catch(function onError(sailsResponse) {
+            console.log(sailsResponse);
+        })
+        .finally(function eitherWay(){
+            $scope.signupForm.location = false;
+        })
     }
 }]);
