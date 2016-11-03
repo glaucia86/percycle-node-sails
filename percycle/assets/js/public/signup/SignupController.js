3430301eb78365d2e32550a8271e6d5793e8e3b8
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2016-11-03 01:24:20 -0200

    Início desenvolvimento do Controller para enviar via Post info do form da página 'Sign Up'.

--- percycle/assets/js/public/signup/SignupController.js
+++ percycle/assets/js/public/signup/SignupController.js
@@ -1,1 +1,5 @@
-angular.module('SignupModule').controller('SignupController', function(){});
+angular.module('SignupModule').controller('SignupController', ['$scope', function($scope){
+    submitSignupForm = function() {
+        console.log("Boa! Deu Tudo certo!!!");
+    }
+}]);
