16a95a8ed609c02e2770198a0176deb2df1b20c5
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2016-11-03 23:17:59 -0200

    Desenvolvimento do 'UserController' para que possa ser chamada a view da Página de Cadastro de Novo Usuário.

--- percycle/api/controllers/UserController.js
+++ percycle/api/controllers/UserController.js
@@ -1,11 +1,14 @@
 /**
- * UserController
- *
- * @description :: Server-side logic for managing users
+ * Arquivo: UserController.js
+ * Data: 03/11/2016
+ * Author: Glaucia Lemos
+ * @description :: Arquivo responsável por manipular o lado dos usuários (lógica) 
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
 module.exports = {
-	
+    'newUser': function (req, res) {
+        res.view();   
+    }
 };
 
