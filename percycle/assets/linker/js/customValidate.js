bc93787411c89bee68ad7b6c95c0ce55f6d443aa
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2016-11-04 01:14:09 -0200

    Adicionando no projeto plugin de validação do JQuery e criação do arquivo para validar os campos no lado do usuário.

---
+++ percycle/assets/linker/js/customValidate.js
@@ -0,0 +1,37 @@
+/**
+ * Arquivo: customValidate.js
+ * Data: 04/11/2016
+ * Author: Glaucia Lemos
+ * @description :: Arquivo responsável por validar os campos no lado do Front-End de maneira mais
+ *  customizada para o usuário.
+ * 
+ */
+
+$(document).ready(function() {
+    // http://bassistance.de/jquery-plugins/jquery-plugin-validation/
+	// http://docs.jquery.com/Plugins/Validation/
+	// http://docs.jquery.com/Plugins/Validation/validate#toptions
+
+    $('.form-signin').validate({
+        rules: {
+            name: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            password: {
+                minlength: 6,
+                required: true
+            },
+            confirmacao: {
+                minlength: 6,
+                equalTo: "#password"
+            }
+        },
+            success: function(e) {
+                e.text('Correto!').addClass('valid')
+            }
+    });
+});
