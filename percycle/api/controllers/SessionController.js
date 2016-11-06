3b844b0ef6dafa950eed210693445109ff3142b1
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2016-11-06 01:30:09 -0200

    Desenvolvimento da validação da senha criptofada na parte da sessão do sistema.

--- percycle/api/controllers/SessionController.js
+++ percycle/api/controllers/SessionController.js
@@ -6,6 +6,8 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
+var bcrypt = require('bcrypt');
+
 module.exports = {
 	
     /** Função responsável por poder manipular as sessões dos usários que se logam no sistema */
@@ -24,7 +26,7 @@ module.exports = {
 
             var usernamePasswordRequiredError = [{
                 name: 'usernamePasswordRequired',
-                message: 'Você deve inserir os campos usuário e senha.'
+                message: 'Você deve inserir os campos e-mail e senha.'
             }]
 
             req.session.flash = {
@@ -36,20 +38,55 @@ module.exports = {
         }
 
         //Aqui estamos procurando o usuário pelo email já cadastrado no sistema:
-        User.findUserByEmail(req.param('email')).done(function(err, user) {
+        User.findOneByEmail(req.param('email'), function foundUser(err, user) {
             if(err)
                 return next(err);
 
             //Caso o usuário não seja encontrado...
             if(!user) {
-                var userNotFound = [{
-                    name: 'notUser',
+                var userNotFoundError = [{
+                    name: 'userNotFound',
                     message: 'O endereço de email ' + req.param('email') + ' não foi encontrado.'
                 }]
+
+                req.session.flash = {
+                    err: userNotFoundError
+                }
 
                 res.redirect('/session/newUser');
                 return;
             }
+
+            /** Aqui iremos comparar do formulário se os parâmetros da senha encriptografada
+             * do usuário foi encontrada
+             */
+            bcrypt.compare(req.param('password'), user.encryptedPassword, function(err, valid) {
+                if(err)
+                    return next(err);
+                
+                //Se a senha digitada no formulário não coincidir com a senha vinda da base de dados...
+                if(!valid) {
+                    var usernamePasswordMismatchError = [{
+                        name: 'usernamePasswordMismatch',
+                        message: 'E-mail e senha inválido.'
+                    }]
+
+                    req.session.flash = {
+                        err: usernamePasswordMismatchError
+                    }
+
+                    res.redirect('/session/newUser');
+                    return;
+                }
+
+                //Caso o usuário consiga se logar será autenticado e....
+                req.session.authenticated = true;
+                req.session.User = user;
+
+                //... será redirecionado para sua página profile pessoal: 
+                //localhost:1337/user/showUser/:id
+                res.redirect('/user/showUser/' + user.id);
+            });
         });
     }
 };
