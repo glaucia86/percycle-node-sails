127b6b95af5d7d1090b3fd5daee3bc207b7de52a
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2016-11-06 00:42:26 -0200

    Desenvolvimento da lógica de controle de sessão do sistema por meio de tempo de autenticação.

--- percycle/api/controllers/SessionController.js
+++ percycle/api/controllers/SessionController.js
@@ -10,6 +10,16 @@ module.exports = {
 	
     /** Função responsável por poder manipular as sessões dos usários que se logam no sistema */
     'newUser': function(req, res) {
+
+        //Aqui estou criando uma variável para poder controlar a sessão através do cookie:
+        //exemplo: http://www.w3schools.com/js/js_dates.asp
+        var oldDateObj = new Date();
+        var newDateObj = new Date(oldDateObj.getTime() + 60000);
+        req.session.cookie.expires = newDateObj;
+
+        req.session.authenticated = true;
+        console.log(req.session);
+
         res.view('session/newUser');
     }
 };
