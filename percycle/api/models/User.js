7a13103c303db7b58dde39255be239cb73e1ae6c
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2016-11-06 02:34:06 -0200

    Definindo um esquema para o Usuário Admin.

--- percycle/api/models/User.js
+++ percycle/api/models/User.js
@@ -23,6 +23,11 @@ module.exports = {
       unique: true
     },
     
+    admin: {
+      type: 'boolean',
+      defaultsTo: false
+    },
+
     encryptedPassword: {
       type: 'string'
     },
